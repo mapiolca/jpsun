23235fe52d6b1b7c277226a1f1d7f41c789756e9
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-02-16 15:44:19 +0100

    Update pdf_jpsunwithoutpictures.modules.php

--- core/modules/propale/doc/pdf_jpsunwithoutpictures.modules.php
+++ core/modules/propale/doc/pdf_jpsunwithoutpictures.modules.php
@@ -883,16 +883,17 @@ class pdf_jpsunwithoutpictures extends ModelePDFPropales
 				}
 				
 				// Add terms to sale
-				if (!empty($mysoc->termsofsale) && getDolGlobalInt('MAIN_PDF_ADD_TERMSOFSALE_PROPAL')) {
-					$termsofsale = $conf->mycompany->dir_output.'/'.$mysoc->termsofsale;
-					if (!empty($conf->mycompany->multidir_output[$object->entity])) {
-						$termsofsale = $conf->mycompany->multidir_output[$object->entity].'/'.$mysoc->termsofsale;
+				if (getDolGlobalString('MAIN_INFO_PROPAL_TERMSOFSALE') && getDolGlobalInt('MAIN_PDF_ADD_TERMSOFSALE_PROPAL')) {
+					$termsofsalefilename = getDolGlobalString('MAIN_INFO_PROPAL_TERMSOFSALE');
+					$termsofsale = $conf->propal->dir_output.'/'.$termsofsalefilename;
+					if (!empty($conf->propal->multidir_output[$object->entity])) {
+						$termsofsale = $conf->propal->multidir_output[$object->entity].'/'.$termsofsalefilename;
 					}
 					if (file_exists($termsofsale) && is_readable($termsofsale)) {
 						$pagecount = $pdf->setSourceFile($termsofsale);
 						for ($i = 1; $i <= $pagecount; $i++) {
 							$tplIdx = $pdf->importPage($i);
-							if ($tplIdx!==false) {
+							if ($tplIdx !== false) {
 								$s = $pdf->getTemplatesize($tplIdx);
 								$pdf->AddPage($s['h'] > $s['w'] ? 'P' : 'L');
 								$pdf->useTemplate($tplIdx);
@@ -902,7 +903,6 @@ class pdf_jpsunwithoutpictures extends ModelePDFPropales
 						}
 					}
 				}
-
 				//If propal merge product PDF is active
 				if (getDolGlobalString('PRODUIT_PDF_MERGE_PROPAL')) {
 					require_once DOL_DOCUMENT_ROOT.'/product/class/propalmergepdfproduct.class.php';
