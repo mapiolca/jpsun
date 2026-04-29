c81d0c8c20d75feb142c57f93f19935ad3985988
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-29 17:46:55 +0200

    fix(sql): remove unsupported boxcode filter on boxes_def cleanup

--- core/modules/modJpsun.class.php
+++ core/modules/modJpsun.class.php
@@ -935,8 +935,8 @@ class modJpsun extends DolibarrModules
 		}
 
 		//$ext->addExtraField($attrname, 02 $label, 03 $type, 04 $pos, 05 $size, 06 $element, 07 $unique, 08 $required, 09 $default_value, 10 $param, 11 $alwayseditable, 12 $perms, 13 $list, 14 $help, 15 $computed, 16 $entity, 17 $langfile, 18 $enabled, 19 $sommable, 20 $PDF)
-		$sql[] = "DELETE FROM ".MAIN_DB_PREFIX."boxes_def WHERE file = 'p.php' AND boxcode = 'p'";
-		$sql[] = "DELETE FROM ".MAIN_DB_PREFIX."boxes_def WHERE file IN ('box_jpsun_pc_install.php','box_jpsun_pc_total_year.php','box_jpsun_pc_monthly.php','box_jpsun_pc_weekly.php') OR boxcode IN ('jpsun_pc_install','jpsun_pc_total_year','jpsun_pc_monthly','jpsun_pc_weekly')";
+		$sql[] = "DELETE FROM ".MAIN_DB_PREFIX."boxes_def WHERE file = 'p.php'";
+		$sql[] = "DELETE FROM ".MAIN_DB_PREFIX."boxes_def WHERE file IN ('box_jpsun_pc_install.php','box_jpsun_pc_total_year.php','box_jpsun_pc_monthly.php','box_jpsun_pc_weekly.php')";
 
 		return $this->_init($sql);
 	}
