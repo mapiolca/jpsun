7014cb2d1dc9348d6176bc8b043f52a0dd6a0d99
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-29 22:56:30 +0200

    fix(widget): use responsive pie height 200/240 per review

--- core/boxes/jpsun_graph_camembert_categorieca.php
+++ core/boxes/jpsun_graph_camembert_categorieca.php
@@ -55,7 +55,7 @@ class jpsun_graph_camembert_categorieca extends ModeleBoxes
 			$px1->SetType(array('pie'));
 				$px1->SetLegend($legend);
 				$px1->setShowLegend(2);
-				$px1->SetHeight(250);
+				$px1->SetHeight(!empty($conf->dol_optimize_smallscreen) ? 200 : 240);
 				if (!empty($conf->dol_optimize_smallscreen)) $px1->SetWidth(320);
 			$px1->SetCssPrefix('cssboxes');
 			$px1->mode = 'depth';
